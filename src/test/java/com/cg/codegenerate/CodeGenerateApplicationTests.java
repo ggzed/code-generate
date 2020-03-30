3e36446d57648ae314933089da032b20ac778332
ggzed <1500510117@qq.com>
2020-03-30 14:18:59 +0800

    一格代码生成器

---
+++ src/test/java/com/cg/codegenerate/CodeGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.codegenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
